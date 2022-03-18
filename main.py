87fc573234d38faf3e1cf9e842d56a7675285841
Fredohm <fredwaucquez@hotmail.fr>
2022-03-18 05:32:27 +0100

    using dynamic typing to format timer

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ def start_timer():
 def count_down(count):
     count_min = floor(count / 60)
     count_sec = (count % 60)
+    # use of dynamic typing
+    if count_min < 10:
+        count_min = f"0{count_min}"
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
@@ -41,7 +47,7 @@ window.config(padx=100, pady=50, bg=YELLOW)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="./tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-timer_text = canvas.create_text(100, 130, text="0", fill="white", font=(FONT_NAME, 36, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 36, "bold"))
 
 
 timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 36, "bold"))
